ffaa26d231808777746a993eba92478fd89c3271
LMACDONALD18 <LMACDONALD18@BLOOMBERG.NET>
2017-05-12 11:02:18 +0100

    highmaps

--- application.py
+++ application.py
@@ -26,6 +26,11 @@ application.add_url_rule('/', 'index', (lambda: header_text +
 application.add_url_rule('/<username>', 'hello', (lambda username:
     header_text + say_hello(username) + home_link + footer_text))
 
+@application.route('/highmaps')
+def highmaps_vis():
+    """ Render a template with highmaps """
+    return render_template("index.html")
+
 # run the app.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
